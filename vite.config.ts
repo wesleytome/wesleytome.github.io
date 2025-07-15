a2a00ade790d0d9d4ab5512fdec5f140392522e6
Wesley <wesleytome@gmail.com>
2025-07-15 13:46:47 -0300

    feat: automatic deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",,
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
